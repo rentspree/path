4c9abaf1e29e2663dc698768fa993d9c54964aaf
Arnan Yodpinit <arnan@rentspree.com>
2019-06-21 21:00:35 +0700

    add back export query

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 // @flow
 
-export { parse, stringify } from "./query-string"
+import { parse, stringify } from "./query-string"
+
 export { buildPath } from "./build-path"
 export { setErrorClass } from "./error"
 export { getSubdomain } from "./get-subdomain"
 export { urlJoin } from "./url-join"
+export { parse, stringify }
+export const query = { parse, stringify }
