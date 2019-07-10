5dcc709b209b682a53c7180bcf9d870ece909b88
sasichapoonpol <sasicha@rentspree.com>
2019-07-10 16:28:39 +0700

    export parseSubdomain

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { parse, stringify } from "./query-string"
 export { buildPath } from "./build-path"
 export { setErrorClass } from "./error"
 export { getSubdomain } from "./get-subdomain"
+export { parseSubdomain } from "./parse-subdomain"
 export { urlJoin } from "./url-join"
 export { parse, stringify }
 export const query = { parse, stringify }
